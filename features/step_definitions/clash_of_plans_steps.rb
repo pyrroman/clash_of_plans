ad5ca0ad5781833d75b0bedb59a273bdbffb3b0c
Bryan Ash <bryan.a.ash@gmail.com>
2014-04-24 18:13:33 -0400

    Step defintions supporting 'I have a level 1 town hall' scenario

---
+++ features/step_definitions/clash_of_plans_steps.rb
@@ -0,0 +1,25 @@
+Given(/^I have a new level (\d+) town hall$/) do |town_hall_level|
+end
+
+When(/^I go to my village page$/) do
+  visit my_village_url
+end
+
+Then(/^I should see town hall 1 buildings available$/) do
+  page.should have_content("Town Hall Level: 1")
+  available_buildings = find("#available_buildings")
+  available_buildings.should have_content("Gold Mine (1)")
+  available_buildings.should have_content("Elixir Collector (1)")
+  available_buildings.should have_content("Gold Storage (1)")
+  available_buildings.should have_content("Elixir Storage (1)")
+  available_buildings.should have_content("Barracks (1)")
+  available_buildings.should have_content("Army Camp (1)")
+end
+
+Given(/^I have a level (\d+) town hall with some upgrades$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see my upgrades separate from the remaining available buildings$/) do
+  pending # express the regexp above with the code you wish you had
+end
