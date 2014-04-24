234031f3bc170da75b338b6375d75f55c243d17c
Bryan Ash <bryan.a.ash@gmail.com>
2014-04-24 19:31:34 -0400

    Extracting some data from the steps

--- features/step_definitions/clash_of_plans_steps.rb
+++ features/step_definitions/clash_of_plans_steps.rb
@@ -1,4 +1,17 @@
-Given(/^I have a new level (\d+) town hall$/) do |town_hall_level|
+Village = Struct.new :town_hall
+TownHall = Struct.new :level, :available_buildings
+Building = Struct.new :name
+
+TownHall1 = TownHall.new(1, [Building.new("Gold Mine"),
+                             Building.new("Elixir Collector"),
+                             Building.new("Gold Storage"),
+                             Building.new("Elixir Storage"),
+                             Building.new("Barracks"),
+                             Building.new("Army Camp")
+                            ])
+
+Given(/^My village has a new level (\d+) town hall$/) do |town_hall_level|
+  @my_village = Village.new(TownHall1.dup)
 end
 
 When(/^I go to my village page$/) do
@@ -6,18 +19,15 @@ When(/^I go to my village page$/) do
 end
 
 Then(/^I should see town hall 1 buildings available$/) do
-  page.should have_content("Town Hall Level: 1")
-  within(".available_buildings") do
-    page.should have_content("Gold Mine (1)")
-    page.should have_content("Elixir Collector (1)")
-    page.should have_content("Gold Storage (1)")
-    page.should have_content("Elixir Storage (1)")
-    page.should have_content("Barracks (1)")
-    page.should have_content("Army Camp (1)")
+  page.should have_content("Town Hall Level: #{@my_village.town_hall.level}")
+  @my_village.town_hall.available_buildings.each do |building|
+    within(".available_buildings") do
+      page.should have_content("#{building.name} (1)")
+    end
   end
 end
 
-Given(/^I have a level (\d+) town hall with some upgrades$/) do |arg1|
+Given(/^My village has a level (\d+) town hall with some upgrades$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
--- features/viewing_my_village.feature
+++ features/viewing_my_village.feature
@@ -1,12 +1,12 @@
 Feature: Viewing my village
 
-  Scenario: I have a new level 1 town hall
-    Given I have a new level 1 town hall
+  Scenario: My village has a new level 1 town hall
+    Given My village has a new level 1 town hall
     When I go to my village page
     Then I should see town hall 1 buildings available
 
   Scenario: I have a level 1 town hall with some upgrades
-    Given I have a level 1 town hall with some upgrades
+    Given My village has a level 1 town hall with some upgrades
     When I go to my village page
     Then I should see my upgrades separate from the remaining available buildings
 
