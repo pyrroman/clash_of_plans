6782754ecb921436467a27f68decded43164cc83
Bryan Ash <bryan.a.ash@gmail.com>
2014-04-24 23:36:21 -0400

    Village viewing sceanrios passing with static data in the view

--- features/step_definitions/clash_of_plans_steps.rb
+++ features/step_definitions/clash_of_plans_steps.rb
@@ -1,17 +1,17 @@
-Village = Struct.new :town_hall
+Village = Struct.new :town_hall_level, :buildings
 TownHall = Struct.new :level, :available_buildings
-Building = Struct.new :name
+Building = Struct.new :name, :level
 
-TownHall1 = TownHall.new(1, [Building.new("Gold Mine"),
-                             Building.new("Elixir Collector"),
-                             Building.new("Gold Storage"),
-                             Building.new("Elixir Storage"),
-                             Building.new("Barracks"),
-                             Building.new("Army Camp")
+TownHall1 = TownHall.new(1, [Building.new("Gold Mine", 1),
+                             Building.new("Elixir Collector", 1),
+                             Building.new("Gold Storage", 1),
+                             Building.new("Elixir Storage", 1),
+                             Building.new("Barracks", 1),
+                             Building.new("Army Camp", 1)
                             ])
 
 Given(/^My village has a new level (\d+) town hall$/) do |town_hall_level|
-  @my_village = Village.new(TownHall1.dup)
+  @my_village = Village.new(1, [])
 end
 
 When(/^I go to my village page$/) do
@@ -19,18 +19,32 @@ When(/^I go to my village page$/) do
 end
 
 Then(/^I should see town hall 1 buildings available$/) do
-  page.should have_content("Town Hall Level: #{@my_village.town_hall.level}")
-  @my_village.town_hall.available_buildings.each do |building|
+  page.should have_content("Town Hall Level: #{@my_village.town_hall_level}")
+  TownHall1.available_buildings.each do |building|
     within(".available_buildings") do
-      page.should have_content("#{building.name} (1)")
+      page.should have_content("#{building.name} (1/1)")
     end
   end
 end
 
 Given(/^My village has a level (\d+) town hall with some upgrades$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @my_village = Village.new(1, [Building.new("Gold Mine", 1),
+                                Building.new("Gold Storage", 1),
+                                Building.new("Barracks", 1),
+                                Building.new("Army Camp", 1)
+                               ])
 end
 
 Then(/^I should see my upgrades separate from the remaining available buildings$/) do
-  pending # express the regexp above with the code you wish you had
+  page.should have_content("Town Hall Level: #{@my_village.town_hall_level}")
+  @my_village.buildings.each do |building|
+    within(".buildings") do
+      page.should have_content("#{building.name} (Level 1)")
+    end
+  end
+  TownHall1.available_buildings.each do |building|
+    within(".available_buildings") do
+      page.should have_content(/#{building.name} \(.\/1\)/)
+    end
+  end
 end
