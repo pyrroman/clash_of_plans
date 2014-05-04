3404de44adce0ec59ef354b275a72bb0c953cb13
Bryan Ash <bryan.a.ash@gmail.com>
2014-05-04 11:26:36 -0400

    villages presenter spec uses real collection class

--- spec/controllers/villages_controller_spec.rb
+++ spec/controllers/villages_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
+require 'clash_of_plans/villages/presenters/collection'
 
 describe VillagesController do
   describe "GET /my_village" do
     it "uses a buildings collection presenter" do
       buildings_presenter = double
 
-      stub_const("ClashOfPlans::Villages::Presenters::Collection", double(for: buildings_presenter))
+      ClashOfPlans::Villages::Presenters::Collection.stub(:for) { buildings_presenter }
 
       get :my_village
       expect(assigns(:buildings)).to be(buildings_presenter)
