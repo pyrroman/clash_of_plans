98a578739ee7c9e336bb9207005f143596772e83
Bryan Ash <bryan.a.ash@gmail.com>
2014-06-28 22:23:07 -0400

    Create buildings controller

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,4 @@
+class BuildingsController < ApplicationController
+  def index
+  end
+end
--- app/controllers/villages_controller.rb
+++
@@ -1,7 +0,0 @@
-require 'villages'
-
-class VillagesController < ApplicationController
-  def my_village
-    @buildings = ClashOfPlans::Villages.my_village
-  end
-end
