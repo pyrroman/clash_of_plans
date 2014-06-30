78d3469114f1a7938d58c2fa1425186eb08a25fa
Bryan Ash <bryan.a.ash@gmail.com>
2014-06-30 14:28:06 -0400

    Fetch upgradable buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,4 +2,7 @@ class Building < ActiveRecord::Base
   def self.upgrading
     where.not(upgrade_started_at: nil)
   end
+  def self.upgradable
+    where(upgrade_started_at: nil).where(level < th_max_level)
+  end
 end
