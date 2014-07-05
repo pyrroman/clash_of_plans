4da0813d9ff7e01e8fedb2e74cb9f0c0962271d3
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-05 17:32:14 +0300

    Add routes for cancel and complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :buildings, only: [:index] do
     put 'upgrade', on: :member
+    put 'cancel', on: :member
+    put 'complete', on: :member
   end
 end
