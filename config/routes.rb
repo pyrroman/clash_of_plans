1d9785569621c26ad3990934d8d4145bd59fc5cc
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-05 18:16:18 +0300

    Fix route paths for cancel and complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :buildings, only: [:index] do
     put 'upgrade', on: :member
-    put 'cancel', on: :member
-    put 'complete', on: :member
+    put 'cancel_upgrade', on: :member
+    put 'complete_upgrade', on: :member
   end
 end
