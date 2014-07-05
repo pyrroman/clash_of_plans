bd4cc492e49e030a92d85457b361bc7a672ecde5
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-05 17:45:22 +0300

    Add cancel and complete model operations

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,11 +2,22 @@ class Building < ActiveRecord::Base
   def self.upgrading
     where.not(upgrade_started_at: nil)
   end
+
   def self.upgradable
     where(upgrade_started_at: nil).where("level < th_max_level")
   end
 
   def start_upgrade
     touch(:upgrade_started_at)
+  end
+
+  def cancel_upgrade
+    update_attribute(:upgrade_started_at, nil)
+  end
+
+  def complete_upgrade
+    self.upgrade_started_at = nil
+    self.level += 1
+    self.save!
   end
 end
