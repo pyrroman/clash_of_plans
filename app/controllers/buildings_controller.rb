f285b832dfeeb4748935b63dd14e66533a96916f
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-06 10:02:59 +0300

    Get the list of maxed buildings

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,17 +2,21 @@ class BuildingsController < ApplicationController
   def index
     @upgrading = Building.upgrading
     @upgradable = Building.upgradable
+    @maxed = Building.maxed
   end
+
   def upgrade
     building = Building.find(params[:id])
     building.start_upgrade
     redirect_to action: :index
   end
+
   def cancel_upgrade
     building = Building.find(params[:id])
     building.cancel_upgrade
     redirect_to action: :index
   end
+
   def complete_upgrade
     building = Building.find(params[:id])
     building.complete_upgrade
