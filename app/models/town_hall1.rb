c021055d605a90dfd32304e41ce414cc8a5e1472
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-06 08:16:15 +0300

    TH2 gets some new buildings
    Need to update the TH max levels of existing buildings

---
+++ app/models/town_hall1.rb
@@ -0,0 +1,10 @@
+class TownHall1 < Building
+  def complete_upgrade
+    Building.create name: "Gold Mine", level: 0, th_max_level: 2, ultimate_level: 11
+    Building.create name: "Elixir Collector", level: 0, th_max_level: 2, ultimate_level: 11
+    Building.create name: "Barracks", level: 0, th_max_level: 3, ultimate_level: 10
+    Building.create name: "Archer Tower", level: 0, th_max_level: 2, ultimate_level: 12
+    self.type = "TownHall2"
+    super
+  end
+end
