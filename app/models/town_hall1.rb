07c0a2851ed818d44a1dffeb43ba404f801d3577
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-08 00:42:58 +0300

    Create specific classes of building

--- app/models/town_hall1.rb
+++ app/models/town_hall1.rb
@@ -1,9 +1,9 @@
 class TownHall1 < Building
   def complete_upgrade
-    Building.create name: "Gold Mine", level: 0, th_max_level: 2, ultimate_level: 11
-    Building.create name: "Elixir Collector", level: 0, th_max_level: 2, ultimate_level: 11
-    Building.create name: "Barracks", level: 0, th_max_level: 3, ultimate_level: 10
-    Building.create name: "Archer Tower", level: 0, th_max_level: 2, ultimate_level: 12
+    GoldMine.create th_max_level: 2
+    ElixirCollector.create th_max_level: 2
+    Barracks.create th_max_level: 3
+    ArcherTower.create th_max_level: 2
     self.type = "TownHall2"
     self.th_max_level = 3
     super
