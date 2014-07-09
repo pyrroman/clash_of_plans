8164c8809f2e355ed92ad5b84b6ea0e3ae4d12ef
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-09 22:43:09 +0300

    TownHall will update th max for all but th10

--- app/models/town_hall0.rb
+++ app/models/town_hall0.rb
@@ -1,17 +1,16 @@
 class TownHall0 < TownHall
   def complete_upgrade
-    GoldMine.create th_max_level: 2
-    ElixirCollector.create th_max_level: 2
-    GoldStorage.create th_max_level: 1
-    ElixirStorage.create th_max_level: 1
-    Barracks.create th_max_level: 3
-    ArmyCamp.create th_max_level: 1
-    Cannon.create th_max_level: 2
+    GoldMine.create
+    ElixirCollector.create
+    GoldStorage.create
+    ElixirStorage.create
+    Barracks.create
+    ArmyCamp.create
+    Cannon.create
 
     Building.update_all_for_th_level(1)
 
     self.type = "TownHall1"
-    self.th_max_level = 2
     super
   end
 end
