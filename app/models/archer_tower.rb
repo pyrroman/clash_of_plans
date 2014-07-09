b1555534fbc8fe249e4f3daa0204a2b9bcf1721b
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-09 22:27:35 +0300

    Define max for th level

--- app/models/archer_tower.rb
+++ app/models/archer_tower.rb
@@ -1,15 +1,11 @@
 class ArcherTower < Building
+  MAX_FOR_LEVEL = { 1: 0, 2: 2, 3: 3, 4: 4, 5: 6, 6: 7, 7: 8, 8: 10, 9: 11, 10: 12 }
+
   def name
     "Archer Tower"
   end
 
   def ultimate_level
     12
-  end
-
-  MAX_FOR_LEVEL = { 1: 0, 2: 2, 3: 3, 4: 4, 5: 6, 7: 8, 8: 10, 9: 11, 10: 12 }
-
-  def update_for_th_level(level)
-    update_attribute(th_max_level: MAX_FOR_LEVEL[level])
   end
 end
