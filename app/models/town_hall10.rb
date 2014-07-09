6a974cae90b085eaaec6d1dac7b50559c3e62b9c
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-09 22:52:29 +0300

    Create town_hall10.rb

---
+++ app/models/town_hall10.rb
@@ -0,0 +1,21 @@
+class TownHall10 < TownHall
+  def complete_upgrade
+    GoldMine.create
+    ElixirCollector.create
+    DarkElixirDrill.create
+    Cannon.create
+    ArcherTower.create
+    GiantBomb.create
+    AirBomb.create
+    SeekingAirMine.create
+    XBow.create
+    InfernoTower.create
+    InfernoTower.create
+
+    Building.update_all_for_th_level(10)
+
+    self.type = "TownHall10"
+    self.th_max_level = 10
+    super
+  end
+end
