364fd10a6584f31de3726f4e97d0821d4a50e48f
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 15:56:28 +0300

    Fix hash syntax, archer tower

--- app/models/archer_tower.rb
+++ app/models/archer_tower.rb
@@ -1,5 +1,5 @@
 class ArcherTower < Building
-  MAX_FOR_LEVEL = { 1: 0, 2: 2, 3: 3, 4: 4, 5: 6, 6: 7, 7: 8, 8: 10, 9: 11, 10: 12 }
+  MAX_FOR_LEVEL = { 1 => 0, 2 => 2, 3 => 3, 4 => 4, 5 => 6, 6 => 7, 7 => 8, 8 => 10, 9 => 11, 10 => 12 }
 
   def name
     "Archer Tower"
