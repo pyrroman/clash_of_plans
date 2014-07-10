0a7a5dde42582521603f007794a3ce2038473131
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 15:59:51 +0300

    Fix hash syntax, town hall

--- app/models/town_hall.rb
+++ app/models/town_hall.rb
@@ -1,5 +1,5 @@
 class TownHall < Building
-  MAX_FOR_LEVEL = { 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10, 10: 10 }
+  MAX_FOR_LEVEL = { 1 => 2, 2 => 3, 3 => 4, 4 => 5, 5 => 6, 6 => 7, 7 => 8, 8 => 9, 9 => 10, 10 => 10 }
 
   def name
     "Town Hall"
