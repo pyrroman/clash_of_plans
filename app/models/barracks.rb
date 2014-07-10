d7049e5274239d6cea0af9541f161b629ee08b2f
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 16:01:14 +0300

    Fix hash syntax, barracks

--- app/models/barracks.rb
+++ app/models/barracks.rb
@@ -1,5 +1,5 @@
 class Barracks < Building
-  MAX_FOR_LEVEL = { 1: 3, 2: 4, 3: 5, 4: 6, 5: 7, 6: 8, 7: 9, 8: 10, 9: 10, 10: 10 }
+  MAX_FOR_LEVEL = { 1 => 3, 2 => 4, 3 => 5, 4 => 6, 5 => 7, 6 => 8, 7 => 9, 8 => 10, 9 => 10, 10 => 10 }
 
   def name
     "Barracks"
