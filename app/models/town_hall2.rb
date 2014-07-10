d2fdffffc19f59cb30076f68f9949764c9e7cdb3
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 23:24:37 +0300

    Create town_hall2.rb

---
+++ app/models/town_hall2.rb
@@ -0,0 +1,17 @@
+class TownHall2 < TownHall
+  def complete_upgrade
+    GoldMine.create
+    ElixirCollector.create
+    GoldStorage.create
+    ElixirStorage.create
+    ArmyCamp.create
+    Laboratory.create
+    Mortar.create
+    Bomb.create
+
+    Building.update_all_for_th_level(3)
+
+    self.type = "TownHall3"
+    super
+  end
+end
