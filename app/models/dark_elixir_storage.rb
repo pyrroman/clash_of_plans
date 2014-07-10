85cfa0c22bd5ffa6ca12d46baa5183949f25797b
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 23:38:20 +0300

    Create dark_elixir_storage.rb

---
+++ app/models/dark_elixir_storage.rb
@@ -0,0 +1,11 @@
+class DarkElixirStorage < Building
+  MAX_FOR_LEVEL = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 2, 8 => 4, 9 => 6, 10 => 6 }
+
+  def name
+    "Dark Elixir Storage"
+  end
+
+  def ultimate_level
+    6
+  end
+end
