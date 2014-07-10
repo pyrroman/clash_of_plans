6297dd50ff5898634ed4ebb38fffdf640822abca
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 15:58:31 +0300

    Fix hash syntax, army camp

--- app/models/army_camp.rb
+++ app/models/army_camp.rb
@@ -1,5 +1,5 @@
 class ArmyCamp < Building
-  MAX_FOR_LEVEL = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 6, 8: 6, 9: 7, 10: 8 }
+  MAX_FOR_LEVEL = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 6, 8 => 6, 9 => 7, 10 => 8 }
 
   def name
     "Army Camp"
