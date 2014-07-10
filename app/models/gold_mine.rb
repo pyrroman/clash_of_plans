fdb24d49b2b6abb24fa5b88a25a05f7b093b3338
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-10 16:12:20 +0300

    Fix hash syntax, gold mine

--- app/models/gold_mine.rb
+++ app/models/gold_mine.rb
@@ -1,5 +1,5 @@
 class GoldMine < Building
-  MAX_FOR_LEVEL = { 1: 2, 2: 4, 3: 6, 4: 8, 5: 10, 6: 10, 7: 11, 8: 11, 9: 11, 10: 11 }
+  MAX_FOR_LEVEL = { 1 => 2, 2 => 4, 3 => 6, 4 => 8, 5 => 10, 6 => 10, 7 => 11, 8 => 11, 9 => 11, 10 => 11 }
 
   def name
     "Gold Mine"
