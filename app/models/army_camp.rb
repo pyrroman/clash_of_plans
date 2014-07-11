08c326c9de59e34ac0c2bb0524198c78dc21cefb
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:50:24 +0300

    Unneeded methods

--- app/models/army_camp.rb
+++ app/models/army_camp.rb
@@ -1,11 +1,3 @@
 class ArmyCamp < Building
   MAX_FOR_LEVEL = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 6, 8 => 6, 9 => 7, 10 => 8 }
-
-  def name
-    "Army Camp"
-  end
-
-  def ultimate_level
-    8
-  end
 end
