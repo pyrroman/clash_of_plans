e2c2438dfb7f81a8d32f5bed54e788f584456f24
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:54:33 +0300

    Unneeded methods

--- app/models/dark_barracks.rb
+++ app/models/dark_barracks.rb
@@ -1,11 +1,3 @@
 class DarkBarracks < Building
   MAX_FOR_LEVEL = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 2, 8 => 4, 9 => 5, 10 => 5 }
-
-  def name
-    "Dark Barracks"
-  end
-
-  def ultimate_level
-    5
-  end
 end
