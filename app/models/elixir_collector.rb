b46e4090574d87c8c8d330cbd717e99e79c3bdd5
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:56:07 +0300

    Unneeded methods

--- app/models/elixir_collector.rb
+++ app/models/elixir_collector.rb
@@ -1,11 +1,3 @@
 class ElixirCollector < Building
   MAX_FOR_LEVEL = { 1 => 2, 2 => 4, 3 => 6, 4 => 8, 5 => 10, 6 => 10, 7 => 11, 8 => 11, 9 => 11, 10 => 11 }
-
-  def name
-    "Elixir Collector"
-  end
-
-  def ultimate_level
-    11
-  end
 end
