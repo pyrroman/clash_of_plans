16926a345630d7b9e9e11c1dd864838201a2d234
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:49:01 +0300

    Unneeded methods

--- app/models/archer_tower.rb
+++ app/models/archer_tower.rb
@@ -1,11 +1,3 @@
 class ArcherTower < Building
   MAX_FOR_LEVEL = { 1 => 0, 2 => 2, 3 => 3, 4 => 4, 5 => 6, 6 => 7, 7 => 8, 8 => 10, 9 => 11, 10 => 12 }
-
-  def name
-    "Archer Tower"
-  end
-
-  def ultimate_level
-    12
-  end
 end
