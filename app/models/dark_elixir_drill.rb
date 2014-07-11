5862523a8a9fc9e6d379afe5d1c0c4e9ee41c5fe
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:54:59 +0300

    Unneeded methods

--- app/models/dark_elixir_drill.rb
+++ app/models/dark_elixir_drill.rb
@@ -1,11 +1,3 @@
 class DarkElixirDrill < Building
   MAX_FOR_LEVEL = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 3, 9 => 6, 10 => 6 }
-
-  def name
-    "Dark Elixir Drill"
-  end
-
-  def ultimate_level
-    6
-  end
 end
