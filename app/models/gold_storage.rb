16ebc0462da68b8820e7be768c59d1703309b320
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:57:51 +0300

    Unneeded methods

--- app/models/gold_storage.rb
+++ app/models/gold_storage.rb
@@ -1,11 +1,3 @@
 class GoldStorage < Building
   MAX_FOR_LEVEL = { 1 => 1, 2 => 3, 3 => 6, 4 => 8, 5 => 9, 6 => 10, 7 => 11, 8 => 11, 9 => 11, 10 => 11 }
-
-  def name
-    "Gold Storage"
-  end
-
-  def ultimate_level
-    11
-  end
 end
