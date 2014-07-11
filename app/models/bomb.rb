4233bbd81fc7843be17442f3bd2b235242744142
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:52:08 +0300

    Unneeded methods

--- app/models/bomb.rb
+++ app/models/bomb.rb
@@ -1,11 +1,3 @@
 class Bomb < Building
   MAX_FOR_LEVEL = { 1 => 0, 2 => 0, 3 => 2, 4 => 2, 5 => 3, 6 => 3, 7 => 4, 8 => 4, 9 => 5, 10 => 5 }
-
-  def name
-    "Bomb"
-  end
-
-  def ultimate_level
-    5
-  end
 end
