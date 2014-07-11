62b578e7b3a25aced3c8f93252c9b8884bcf9eb6
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 11:53:36 +0300

    Unneeded methods

--- app/models/cannon.rb
+++ app/models/cannon.rb
@@ -1,11 +1,3 @@
 class Cannon < Building
   MAX_FOR_LEVEL = { 1 => 2, 2 => 3, 3 => 4, 4 => 5, 5 => 6, 6 => 7, 7 => 8, 8 => 10, 9 => 11, 10 => 12 }
-
-  def name
-    "Cannon"
-  end
-
-  def ultimate_level
-    12
-  end
 end
