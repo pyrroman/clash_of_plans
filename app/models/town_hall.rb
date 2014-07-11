1f8a2ecc039138d08362b6352b32642b9c29a801
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-11 12:03:45 +0300

    Unneeded method

--- app/models/town_hall.rb
+++ app/models/town_hall.rb
@@ -1,11 +1,8 @@
 class TownHall < Building
   MAX_FOR_LEVEL = { 1 => 2, 2 => 3, 3 => 4, 4 => 5, 5 => 6, 6 => 7, 7 => 8, 8 => 9, 9 => 10, 10 => 10 }
 
+  # All Town Hall levels have the same name
   def name
     "Town Hall"
-  end
-
-  def ultimate_level
-    10
   end
 end
