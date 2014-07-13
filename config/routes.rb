b46ebc37ba6b990625224f041fbc3892f33e5020
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-13 05:55:32 -0400

    Add buildings as a nested resource of villages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :buildings, only: [:index] do
-    put 'upgrade', on: :member
-    put 'cancel_upgrade', on: :member
-    put 'complete_upgrade', on: :member
+  resources :villages, only: [:index] do
+    resources :buildings, only: [:index] do
+      put 'upgrade', on: :member
+      put 'cancel_upgrade', on: :member
+      put 'complete_upgrade', on: :member
+    end
   end
 end
