ea0971a623d112fed65faee0bf2b23931ba79d67
Bryan Ash <bryan.a.ash@gmail.com>
2014-07-13 06:57:34 -0400

    Un-nest the building upgrade routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :villages, only: [:index] do
-    resources :buildings, only: [:index] do
-      put 'upgrade', on: :member
-      put 'cancel_upgrade', on: :member
-      put 'complete_upgrade', on: :member
-    end
+  resources :villages, only: [:index]
+    resources :buildings, only: [:index]
+  end
+
+  resources :buildings do
+    put 'upgrade', on: :member
+    put 'cancel_upgrade', on: :member
+    put 'complete_upgrade', on: :member
   end
 end
