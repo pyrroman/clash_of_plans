28b4f1e77fd0be4da89973a891c99a14d7167c34
Bryan Ash <bryan.a.ash@gmail.com>
2014-08-04 12:45:08 -0400

    remove temporary village assignment

--- app/controllers/villages_controller.rb
+++ app/controllers/villages_controller.rb
@@ -1,9 +1,5 @@
 class VillagesController < ApplicationController
   def my_village
-    if current_user.villages.empty?
-      current_user.villages << Village.first
-    end
-
     @village = current_user.villages.first
   end
 end
